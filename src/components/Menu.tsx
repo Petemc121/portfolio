1fb75e736c13cd04eff3f70ba570b1f4309f5c20
Petemc121 <petemc121@gmail.com>
2022-01-01 22:52:19 +0000

    updated menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 interface menuInput {
   menuActive: boolean;
@@ -6,15 +8,24 @@ interface menuInput {
 }
 
 export default function Menu({ menuActive, setMenuActive }: menuInput) {
+  console.log("menu" + menuActive);
   const deactivateMenu = () => {
     setMenuActive(false);
   };
   return (
-    <div
-      onClick={deactivateMenu}
-      className={menuActive ? "activeMenu menu" : "menu"}
-    >
-      hello
+    <div className={menuActive ? "activeMenu menu" : "menu"}>
+      <FontAwesomeIcon
+        onClick={deactivateMenu}
+        className="exitMenu"
+        icon={faTimes}
+        size="2x"
+      />
+      <div className="menuListContainer">
+        <div className="menuItem">My work</div>
+        <div className="menuItem">About me</div>
+        <div className="menuItem">Blog</div>
+        <div className="menuItem">Contact</div>
+      </div>
     </div>
   );
 }
