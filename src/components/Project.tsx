0d3ed89fa676276e987abf081fb3c1b21f10e433
Petemc121 <petemc121@gmail.com>
2022-01-02 18:12:15 +0000

    added links and animations to projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GoMarkGithub } from "react-icons/go";
 
 interface projectInput {
   project: {
@@ -7,30 +8,43 @@ interface projectInput {
     img: any;
     title: string;
     description: string;
+    link: string;
+    github: string;
     setProjectHovered: any;
     projectHovered: number | null;
   };
 }
 
 export default function Project({ project }: projectInput) {
-  const handleProjectHoverIn = (e: any) => {
-    e.target.style.opacity = "1";
+  const handleProjectHoverIn = () => {
+    project.setProjectHovered(project.id);
   };
-
   const handleProjectHoverOut = (e: any) => {
-    e.target.style.opacity = "0";
+    project.setProjectHovered(null);
   };
   return (
     <div className={"project " + project.className}>
-      <img
+      <a className="links" href={project.link}>
+        <img
+          onMouseOver={handleProjectHoverIn}
+          onMouseOut={handleProjectHoverOut}
+          style={{ opacity: project.projectHovered === project.id ? "1" : "0" }}
+          src={project.img}
+          className="projectImage"
+        ></img>
+      </a>
+      <div
         onMouseOver={handleProjectHoverIn}
         onMouseOut={handleProjectHoverOut}
-        src={project.img}
-        className="projectImage"
-      ></img>
-      <div className="projectText">
-        <div className="projectTitle">{project.title}</div>
+        className="projectText"
+      >
+        <a className="links" href={project.link}>
+          <div className="projectTitle">{project.title}</div>
+        </a>
         <div className="projectDescription">{project.description}</div>
+        <a className="links" href={project.github}>
+          <GoMarkGithub className="githubIcon" size="30" />
+        </a>
       </div>
     </div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,6 +17,8 @@ export default function Projects() {
       img: roamingrolls,
       title: "RoamingRolls",
       description: "A jiujitsu gym database website.",
+      link: "https://www.roamingrolls.com/",
+      github: "https://github.com/Petemc121/RoamingRolls",
       setProjectHovered: setProjectHovered,
       projectHovered: projectHovered,
     },
@@ -27,6 +29,8 @@ export default function Projects() {
       title: "Silverbacks MMA",
       description:
         "A website built with React for a jiujitsu gym business owner.",
+      link: "https://silverbacks-mma.herokuapp.com/",
+      github: "https://github.com/Petemc121/silverbacks-mma",
       setProjectHovered: setProjectHovered,
       projectHovered: projectHovered,
     },
@@ -37,6 +41,8 @@ export default function Projects() {
       title: "Covid Updates",
       description:
         "An API fetching web-app built with React displaying covid data through dynamic graphs and animations.",
+      link: "https://corona-tracker-101.netlify.app/",
+      github: "https://github.com/Petemc121/corona-updates",
       setProjectHovered: setProjectHovered,
       projectHovered: projectHovered,
     },
@@ -47,6 +53,8 @@ export default function Projects() {
       title: "Mat Tracker",
       description:
         "A PERN stack built web-app that logs members of a jiujitsu gym in it's database",
+      link: "https://mat-tracker.herokuapp.com/",
+      github: "https://github.com/Petemc121/mat-tracker",
       setProjectHovered: setProjectHovered,
       projectHovered: projectHovered,
     },
@@ -57,6 +65,8 @@ export default function Projects() {
       title: "BJJ Notes",
       description:
         "A web-app built with React that logs jiujitsu techniques with drag and drop functionality.",
+      link: "https://petemc121.github.io/bjj-notes-app/",
+      github: "https://github.com/Petemc121/bjj-notes-app",
       setProjectHovered: setProjectHovered,
       projectHovered: projectHovered,
     },
@@ -66,6 +76,8 @@ export default function Projects() {
       img: portfolio,
       title: "My Portfolio",
       description: "This very portfolio was also built with React!",
+      link: "https://petes-portfolio.herokuapp.com/",
+      github: "https://github.com/Petemc121/portfolio",
       setProjectHovered: setProjectHovered,
       projectHovered: projectHovered,
     },
